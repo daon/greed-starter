f0e44e85a3219110ae5c24fb934e434575873009
Dan Andersson <dan.andersson@outlook.com>
2017-05-23 00:12:20 +0200

    Add build scripts and boilerplate code

---
+++ src/greed.js
@@ -0,0 +1,3 @@
+export const NAME = 'Greed';
+
+/* Write your code here */
---
+++ src/greed.test.js
@@ -0,0 +1,14 @@
+import expect from 'expect';
+import * as greed from './greed';
+
+describe('Greed', () => {
+
+    describe('Name', () => {
+        it('should be equal to Greed', () => {
+            expect(greed.NAME).toEqual('Greed');
+        });
+    });
+
+    /* Write your tests here */
+
+});
