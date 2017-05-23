9af48e211b3b4564df3afaee8a5e0740e89a3d1b
Dan Andersson <dan.andersson@outlook.com>
2017-05-23 22:26:10 +0200

    Add code from OA Code Night

--- src/greed.js
+++ src/greed.js
@@ -1,3 +1,77 @@
 export const NAME = 'Greed';
 
+const scoringRules = [
+    function singleOne(diceAnalysis) {
+        if (diceAnalysis[1] > 0) {
+            let myDiceAnalysis = Object.assign({}, diceAnalysis);
+            myDiceAnalysis[1]--;
+            return {
+                diceAnalysis: myDiceAnalysis,
+                score: 100
+            }
+        } else {
+            return {
+                diceAnalysis,
+                score: 0
+            }
+        }
+    },
+    function tripleOne(diceAnalysis) {
+        if (diceAnalysis[1] >= 3) {
+            let myDiceAnalysis = Object.assign({}, diceAnalysis);
+            myDiceAnalysis[1]-=3;
+            return {
+                diceAnalysis: myDiceAnalysis,
+                score: 1000
+            }
+        } else {
+            return {
+                diceAnalysis,
+                score: 0
+            }
+        }
+    }
+]
+
+function isElementValid(digit) {
+    return digit >= 1 && digit <= 6;
+}
+
+export function score(dice) {
+    if (
+        !Array.isArray(dice)
+        || dice.length !== 5
+        || !dice.every(isElementValid)
+    ) {
+        return "invalid input";
+    }
+    else {
+        return scoreRecursively(analyzeDice(dice));
+    }
+}
+
+export function analyzeDice(dice) {
+    return dice.reduce((previousValue, currentValue) => {
+        if (previousValue[currentValue]) {
+            previousValue[currentValue]++;
+        } else {
+            previousValue[currentValue] = 1;
+        }
+        return previousValue;
+    }, {});
+}
+
+function scoreRecursively(diceAnalysis) {
+    const ruleResults = scoringRules.map(fn => fn(diceAnalysis));
+
+    let sortedResults = ruleResults.sort((a, b) => b.score - a.score);
+    if (sortedResults[0].score > 0) {
+        return sortedResults[0].score + scoreRecursively(sortedResults[0].diceAnalysis);
+    }
+    else {
+        return sortedResults[0].score;
+    }
+}
+
+
 /* Write your code here */
--- src/greed.test.js
+++ src/greed.test.js
@@ -9,6 +9,60 @@ describe('Greed', () => {
         });
     });
 
+    describe('analyzeDice', () => {
+        it('array of valid input should return object describing data', () => {
+            expect(greed.analyzeDice([1, 1, 1, 2, 3])).toEqual({ 1: 3, 2: 1, 3: 1 });
+        });
+    });
+
+    describe('score', () => {
+        it('string input returns invalid input', () => {
+            expect(greed.score("")).toEqual('invalid input');
+            expect(greed.score("1,2,3,4,5")).toEqual('invalid input');
+            expect(greed.score("12345")).toEqual('invalid input');
+        });
+
+        it('array of five valid elements returns positive numeric', () => {
+            expect(typeof greed.score([1, 2, 3, 4, 5])).toEqual('number');
+        });
+
+        it('array of zero elements returns invalid input', () => {
+            expect(greed.score([])).toEqual('invalid input');
+        });
+
+        it('array of four valid elements returns invalid input', () => {
+            expect(greed.score([1,2,3,4])).toEqual('invalid input');
+        });
+
+        it('array of six valid elements returns invalid input', () => {
+            expect(greed.score([1,2,3,4,5,6])).toEqual('invalid input');
+        });
+
+        it('returns invalid input if any element doesnt match 1..6', () => {
+            expect(greed.score([1,-2,3,4,6])).toEqual('invalid input');
+            expect(greed.score([null,2,4,5,6])).toEqual('invalid input');
+            expect(greed.score([2,3,14,5,6])).toEqual('invalid input');
+            expect(greed.score([1,2,undefined,5,6])).toEqual('invalid input');
+            expect(greed.score([6,2,NaN,5,4])).toEqual('invalid input');
+            expect(greed.score([6,"D",3,5,4])).toEqual('invalid input');
+        });
+
+        it('applies rule "single 1" correctly', () => {
+            expect(greed.score([1, 2, 3, 4, 6])).toEqual(100);
+        });
+
+        it('applies rule "triple 1" correctly', () => {
+            expect(greed.score([1, 1, 1, 4, 6])).toEqual(1000);
+        });
+
+        it('applies rule "triple 1" with "single 1" correctly', () => {
+            expect(greed.score([1, 1, 1, 1, 6])).toEqual(1100);
+        });
+
+});
+
+
+
     /* Write your tests here */
 
 });
